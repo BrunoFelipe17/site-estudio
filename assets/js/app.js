fc2c8e6db4260d7f19098900ab0e76071cf13c0f
Bruno Felipe <bruno.felipe7170@gmail.com>
2022-01-05 20:35:22 -0300

    First commit

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+// window.onload = function () {
+//   const iframeInstagram = document.querySelector("iframe");
+//   console.log(iframeInstagram);
+//   // iframeInstagram.innerHTML += "<style>.css-11cewt9 {display: none}</style>";
+// };
+
+(function () {
+  new InstagramFeed({
+    username: "instagram",
+    container: document.getElementById("instagram-feed"),
+    display_profile: true,
+    display_biography: true,
+    display_gallery: true,
+    display_captions: true,
+    max_tries: 8,
+    callback: null,
+    styling: true,
+    items: 8,
+    items_per_row: 4,
+    margin: 1,
+    lazy_load: true,
+    on_error: console.error,
+  });
+})();
